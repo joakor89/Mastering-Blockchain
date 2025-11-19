f927e19a5495b6a4e82a7d2bfd86c4e015af7f49
joakor89 <joacorf33@gmail.com>
2025-11-19 11:34:37 -0400

    Symmetric Encription: Generating random strings

---
+++ chap_03/03_SC.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ======================================================
+# Author: Joaquin Romero
+# Script: AES Encryption Demo
+# Description: Encrypts and decrypts a test file using AES-256.
+# ======================================================
+
