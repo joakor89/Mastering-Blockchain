7e7d81c3f34e87268d05f4cc091cc264e83fbf8f
joakor89 <joacorf33@gmail.com>
2025-11-20 10:34:35 -0400

    Encrypting messages with SHA-256

--- chap_03/03_SC.sh
+++ chap_03/03_SC.sh
@@ -6,5 +6,16 @@
 # Description: Encrypts and decrypts a test file using AES-256.
 # ======================================================
 
+#### Generating random strings
+
 openssl rand -hex 16
+
+openssl help
+
+#### Encrypting messages with SHA-256
+
+echo -n 'Hello' | openssl dgst -sha256
+
+echo -n 'hello' | openssl dgst -sha256
+
 
