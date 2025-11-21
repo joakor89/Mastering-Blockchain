eaa4dfa9744c567b01bba3ad857b642a8748b3b8
joakor89 <joacorf33@gmail.com>
2025-11-21 09:31:03 -0400

    Encrypting and decrypting using AES

--- chap_03/03_SC.sh
+++ chap_03/03_SC.sh
@@ -6,7 +6,7 @@
 # Description: Encrypts and decrypts a test file using AES-256.
 # ======================================================
 
-#### Generating random strings
+#### Generating Random Strings
 
 openssl rand -hex 16
 
@@ -17,5 +17,19 @@ openssl help
 echo -n 'Hello' | openssl dgst -sha256
 
 echo -n 'hello' | openssl dgst -sha256
+
+### Advanced Encryption Standard
+
+#### Encrypting and Decrypting using AES
+
+echo Datatoencrypt > message.txt
+
+openssl enc -aes-256-cbc -in message.txt -out message.bin
+
+openssl enc -base64 -in message.bin -out message.b64
+
+cat message.b64
+
+openssl enc -d -aes-256-cbc -in message.bin -out message.dec
 
 
