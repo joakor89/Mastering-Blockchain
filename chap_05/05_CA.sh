ce2cb63fec47cf64d26fc17731d3647c87d795e2
joakor89 <joacorf33@gmail.com>
2025-11-25 09:04:17 -0400

    Consensus Algorithms Demo

---
+++ chap_05/05_CA.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# ======================================================
+# Author: Joaquin Romero
+# Script: Consensus Algorithms Demo
+# Description: Demonstrates foundational concepts of blockchain
+#              consensus mechanisms, including PoW (Proof of Work),
+#              PoS (Proof of Stake), and other distributed agreement models.
+# ======================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
