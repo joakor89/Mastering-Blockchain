3f04255e00bb9999ab35793a3dd6e498d5424ca8
joakor89 <joacorf33@gmail.com>
2025-12-02 13:53:00 -0400

    Bitcoin payments

--- chap_06/06_BA.sh
+++ chap_06/06_BA.sh
@@ -11,3 +11,7 @@ bitcoin-cli getrawtransaction
 
 #### Proof of Work (PoW)
 bitcoin-cli getdifficulty
+
+#### Wallets
+file wallet.dat wallet.dat: Berkeley DB (Btree, version 9, native byte-order)
+
