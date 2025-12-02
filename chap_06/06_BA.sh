49f8ce601a087a769c1445e73225803d32f7311e
joakor89 <joacorf33@gmail.com>
2025-12-01 21:50:12 -0400

    Bitcoin Architecture

--- chap_06/06_BA.sh
+++ chap_06/06_BA.sh
@@ -1,3 +1,13 @@
-#
+#!/bin/bash
+
+# ======================================================
+# Author: Joaquin Lugo
+# Script: Bitcoin Architecture
+# Description: 
+# ======================================================
 
 bitcoin-cli getrawtransaction
+
+
+#### Proof of Work (PoW)
+bitcoin-cli getdifficulty
