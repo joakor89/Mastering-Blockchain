383ac9134828205373d96db8aebf9092e3f86525
joakor89 <joacorf33@gmail.com>
2025-12-03 12:25:06 -0400

    Experimenting further with bitcoin-cli

--- chap_07/07_BIP.sh
+++ chap_07/07_BIP.sh
@@ -36,4 +36,44 @@ bitcoin-cli --testnet help
 
 bitcoin-cli --testnet stop
 
-#### 
+#### Starting up a node in regtest
+
+bitcoind -regtest -daemon
+
+bitcoin-cli -regtest getbalance
+
+bitcoin-cli -regtest generatetoaddress 200 $(bitcoin-cli -regtest getnewaddress)
+
+bitcoin-cli -regtest getbalance
+
+bitcoin-cli -regtest getmininginfo
+
+bitcoin-cli -regtest getblockchaininfo
+
+bitcoin-cli -regtest stop
+
+#### Experimenting further with bitcoin-cli
+
+bitcoin-cli -regtest getnewaddress
+
+bitcoin-cli -regtest sendtoaddress b1546b6ebe7c2fcf37c9f8c34fbae2a72a6f0dd817b305e0b72bfd18729c6452 20.00
+
+bitcoin-cli -regtest generatetoaddress 7 $(bitcoin-cli -regtest getnewaddress)
+
+#### Using the Bitcoin command-line Tool
+
+bitcoin-cli getblock 
+
+#### Using the JSON-RPC interface
+
+cat bitcoin.conf
+
+curl --user test1:testpassword \
+  --data-binary '{"jsonrpc":"1.0","id":"rawtx","method":"getrawtransaction","params":["b1546b6ebe7c2fcf37c9f8c34fbae2a72a6f0dd817b305e0b72bfd18729c6452", true]}' \
+  -H 'content-type: text/plain;' \
+  http://127.0.0.1:18443/
+
+#### Using the HTTP REST interface
+
+curl http://127.0.0.1:18443/rest/block/4cfefb45cd6d0a62e35fe0f3c17abb5a64a1f50cbed276bdee3fbd771f701afe.json
+
