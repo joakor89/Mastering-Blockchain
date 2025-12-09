47c0305f1f4363c87411db5b2987842f792e53d3
joakor89 <joacorf33@gmail.com>
2025-12-09 13:57:58 -0400

    Geth JSON RPC API

--- chap_10/10_EIP.sh
+++ chap_10/10_EIP.sh
@@ -14,16 +14,47 @@ geth
 
 #### Creating a Geth new account
 
- geth account new
+geth account new
 
- geth account list
+geth account list
 
  #### Geth console
 
- geth console
+geth console
 
  #### Geth attach
 
- geth attach
+geth attach
 
- eth.accounts
+eth.accounts
+
+ #### Geth JSON RPC API
+
+geth --http --http.api "eth,net,web3,personal"
+geth --dev --http --http.api "eth,net,web3,debug,txpool,miner"
+
+curl -X POST --insecure \
+    -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"eth_accounts","params":[], "id":64}'\
+    http://localhost:8545
+
+curl -X POST --insecure \
+    -H "Content-Type: application/json" \
+    --data '{"jsonrpc":"2.0","method":"net_listening","params":[], "id":64}'\ 
+    http://localhost:8545
+
+curl -X POST --insecure \
+    -H "Content-Type: application/json"\
+    --data '{"jsonrpc":"2.0","method":"web3_clientVersion","params":[], "id":64}'\ 
+    http://localhost:8545
+
+curl -X POST --insecure \
+    -H "Content-Type: application/json"\ 
+    --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[], "id":64}'\ 
+    http://localhost:8545
+
+curl -X POST --insecure \
+    -H "Content-Type: application/json"\ 
+    --data '{"jsonrpc":"2.0","method":"eth_coinbase","params":[], "id":64}'\ 
+    http://localhost:8545
+
