3892209b07a51aa12aad7c3eea6c946f947e1887
joakor89 <joacorf33@gmail.com>
2025-12-12 05:38:30 -0400

    Tools Language & Frameworks for Ethereum Devs.

---
+++ chap_11/11_TLFED.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ======================================================
+# Author: Joaquin Lugo
+# Script: Tools Language & Frameworks for Ethereum Devs.
+# Description: Ethereum smart contract development.
+# ======================================================
+
+#### Installing and configuring the Geth client
