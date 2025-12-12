6adc5977a2e699ac52d833bd6f41afb9d30280af
joakor89 <joacorf33@gmail.com>
2025-12-12 10:38:48 -0400

    The Solidity language

--- chap_11/Addition.sol
+++ chap_11/Addition.sol
@@ -1,5 +1,3 @@
-
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -14,3 +12,25 @@ contract Addition {
         return x;
     }
 }
+
+### The Solidity language
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+contract valueChecker {
+    uint8 price=10;
+    //price variable declared and initialized with a value of 10
+    event valueEvent(bool returnValue);
+    
+    function Matcher(uint8 x) public returns (bool y)
+    {
+        if (x>=price)
+        {
+            emit valueEvent(true);
+            y= true;
+        }
+    }
+}
+
+#### Functions
