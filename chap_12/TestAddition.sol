dccca23192fb14a34b4d3fe78eb33c17dbacb4c6
joakor89 <joacorf33@gmail.com>
2025-12-16 08:48:11 -0400

    TestAddition: Indentation,duplicability and space fixed

---
+++ chap_12/TestAddition.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.2;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Addition.sol";
+
+contract TestAddition {
+
+    function testAddition() public {
+        Addition adder = Addition(DeployedAddresses.Addition());
+
+        adder.addx(100, 100);
+
+        uint returnedResult = adder.retrievex();
+        uint expected = 200;
+
+        Assert.equal(returnedResult, expected, "should result 200");
+    }
+}
+
