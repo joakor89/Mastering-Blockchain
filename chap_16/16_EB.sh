16ff736750f8daab902fb92d22464248864743d8
joakor89 <joacorf33@gmail.com>
2025-12-23 11:39:38 -0400

    Enterprice Blockchain

---
+++ chap_16/16_EB.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ======================================================
+# Author: Joaquin Lugo
+# Script: Ethereum Enterprice
+# Description: 
+# ======================================================
+
+
+# Enterprice Blockchain
+
+### Setting up a Quorum network with IBFT
+
+#### Installing and running Quorum Wizard
+
+npm -v
+
+node –v
+
+
+
