6acae43e43c0029f7a706c4a3cbc093cad31e955
joakor89 <joacorf33@gmail.com>
2025-12-24 08:35:36 -0400

    Node 1

--- chap_16/16_EB.sh
+++ chap_16/16_EB.sh
@@ -29,5 +29,22 @@ cd network/4-nodes-istanbul-tessera-bash
 
 4-nodes-istanbul-tessera-bash ./runscript.sh private_contract.js
 
+#### Attaching Geth to nodes
 
+geth attach qdata/dd1/geth.ipc
+
+var abi =
+[{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"",
+"type":"uint256"}],"payable":false,"type":"function"},{"constant":false,
+"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],
+"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get",
+"outputs":[{"name":"retVal","type":"uint256"}],"payable":false,
+"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],
+"payable":false,"type":"constructor"}];
+
+var simpleContract = eth.contract(abi)
+
+var simple = simpleContract.at("0x9d13c6d3afe1721beef56b55d303b09e021e27ab")
+
+simple.get()
 
