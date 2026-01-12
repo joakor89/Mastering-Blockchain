26f70600a1ba8fb509cda711480833473fc3c420
joakor89 <joacorf33@gmail.com>
2026-01-12 08:15:31 -0400

    Blockchain Applications and What’s Next

---
+++ chap_22/22_BAWN.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ======================================================
+# Author: Joaquin Lugo
+# Script: Blockchain Applications and What’s Next
+# Description: Iot, Governance, DeFi & so on
+# ======================================================
+
+
+# Blockchain Applications and What’s Next
+
+### Setting up Raspberry Pi
+
+uname -a
+
+
